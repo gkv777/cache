94e890c428032d7e11e47d9dbbb60aabc9f48a66
Konstantin Gaydukov <k.gaidukov@gmail.com>
2022-05-15 16:47:32 +0300

    добавлен core без mutex

---
+++ core/lru.go
@@ -0,0 +1,101 @@
+package core
+
+import (
+	"container/list"
+
+	"github.com/gkv777/cache"
+)
+
+// По условиям задания (интерфейс LRUCache) и ключ и значение - строки
+type item struct {
+	key   string
+	value string
+}
+
+type lru struct {
+	cap   int
+	items map[string]*list.Element // item содержится соотв. в list.Element
+	queue *list.List
+}
+
+func NewLRUCache(n int) (cache.LRUCache, error) {
+	if n <= 0 {
+		return nil, cache.ErrCapSize
+	}
+	return &lru{
+		cap:   n,
+		items: make(map[string]*list.Element),
+		queue: list.New(),
+	}, nil
+}
+
+func (l *lru) Add(key, value string) bool {
+	if _, ok := l.items[key]; ok {
+		return false
+	}
+	// Если
+	if l.queue.Len() == l.cap {
+		l.removeLast()
+	}
+	// Добавляем в начало списка новые данне
+	i := &item{
+		key:   key,
+		value: value,
+	}
+	e := l.queue.PushFront(i)
+
+	l.items[key] = e
+	return true
+}
+
+func (l *lru) Get(key string) (value string, ok bool) {
+
+	e, ok := l.items[key]
+	if !ok {
+		return "", false
+	}
+
+	// Перемещаем связанный с item элемент списка в начало
+	l.queue.MoveToFront(e)
+	return e.Value.(*item).value, true
+}
+
+func (l *lru) Remove(key string) (ok bool) {
+
+	e, ok := l.items[key]
+	if !ok {
+		return false
+	}
+
+	l.queue.Remove(e)
+	delete(l.items, key)
+	return true
+}
+
+// Вытеснение (удаление) последнего элемента очереди (протухшие данные)
+func (l *lru) removeLast() {
+	//l.Lock()
+	//defer l.Unlock()
+
+	// получаем последний элемент и удаляем
+	if e := l.queue.Back(); e != nil {
+		l.queue.Remove(e)
+		delete(l.items, e.Value.(*item).key)
+	}
+}
+
+func (l *lru) getFirst() *item {
+	return l.queue.Front().Value.(*item)
+}
+
+func (l *lru) getLast() *item {
+	return l.queue.Back().Value.(*item)
+}
+
+func (l *lru) Cap() int {
+	return l.cap
+}
+
+func (l *lru) Len() int {
+	return l.queue.Len()
+}
---
+++ core/lru_test.go
@@ -0,0 +1,134 @@
+package core
+
+import (
+	"container/list"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func newLru(t *testing.T) *lru {
+	return &lru{
+		cap:   3,
+		items: make(map[string]*list.Element),
+		queue: list.New(),
+	}
+}
+
+func TestNewLRUCache(t *testing.T) {
+	l, err := NewLRUCache(4)
+	require.NoError(t, err)
+	require.Equal(t, 4, l.Cap())
+	require.Equal(t, 0, l.Len())
+	l, err = NewLRUCache(0)
+	require.Error(t, err)
+	require.Equal(t, nil, l)
+}
+
+func Test_lruAdd(t *testing.T) {
+	l := newLru(t)
+
+	t.Run("key1", func(t *testing.T) {
+		ok := l.Add("key1", "value1")
+		require.Equal(t, true, ok)
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, l.getFirst().key, "key1")
+		require.Equal(t, l.getLast().key, "key1")
+	})
+
+	t.Run("key2", func(t *testing.T) {
+		ok := l.Add("key2", "value2")
+		require.Equal(t, true, ok)
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, l.getFirst().key, "key2")
+		require.Equal(t, l.getLast().key, "key1")
+	})
+
+	t.Run("key2_duplicate", func(t *testing.T) {
+		ok := l.Add("key2", "value2")
+		require.Equal(t, false, ok)
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, l.getFirst().key, "key2")
+		require.Equal(t, l.getLast().key, "key1")
+	})
+
+	t.Run("key3", func(t *testing.T) {
+		ok := l.Add("key3", "value3")
+		require.Equal(t, true, ok)
+		require.Equal(t, 3, l.Len())
+		require.Equal(t, l.getFirst().key, "key3")
+		require.Equal(t, l.getLast().key, "key1")
+	})
+
+	t.Run("key4", func(t *testing.T) {
+		ok := l.Add("key4", "value4")
+		require.Equal(t, true, ok)
+		require.Equal(t, 3, l.Len())
+		require.Equal(t, l.getFirst().key, "key4")
+		require.Equal(t, l.getLast().key, "key2")
+	})
+}
+
+func Test_lruGet(t *testing.T) {
+	items := []struct {
+		key string
+		val string
+	}{{"key1", "value1"},{"key2", "value2"},{"key3", "value3"},{"key4", "value4"},}
+
+	l := newLru(t)
+	for _, i := range items {
+		l.Add(i.key, i.val)
+	}
+
+	t.Run("key1", func(t *testing.T) {
+		val, ok := l.Get("key1")
+		require.Equal(t, false, ok)
+		require.Equal(t, "", val)
+		require.Equal(t, "key4", l.getFirst().key)
+		require.Equal(t, "key2", l.getLast().key)
+	})
+
+	t.Run("key3", func(t *testing.T) {
+		val, ok := l.Get("key3")
+		require.Equal(t, true, ok)
+		require.Equal(t, "value3", val)
+		require.Equal(t, "key3", l.getFirst().key)
+		require.Equal(t, "key2", l.getLast().key)
+	})
+}
+
+func Test_lruRemove(t *testing.T) {
+	items := []struct {
+		key string
+		val string
+	}{{"key1", "value1"},{"key2", "value2"},{"key3", "value3"},{"key4", "value4"},}
+
+	l := newLru(t)
+	for _, i := range items {
+		l.Add(i.key, i.val)
+	}
+
+	t.Run("key1", func(t *testing.T) {
+		ok := l.Remove("key1")
+		require.Equal(t, false, ok)
+		require.Equal(t, 3, l.Len())
+		require.Equal(t, "key4", l.getFirst().key)
+		require.Equal(t, "key2", l.getLast().key)
+	})
+
+	t.Run("key3", func(t *testing.T) {
+		ok := l.Remove("key3")
+		require.Equal(t, true, ok)
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, "key4", l.getFirst().key)
+		require.Equal(t, "key2", l.getLast().key)
+	})
+
+	t.Run("key3_after", func(t *testing.T) {
+		ok := l.Remove("key3")
+		require.Equal(t, false, ok)
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, "key4", l.getFirst().key)
+		require.Equal(t, "key2", l.getLast().key)
+	})
+}
